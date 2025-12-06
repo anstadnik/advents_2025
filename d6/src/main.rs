94fc363a7dcdfa034eddb9b3b80306f6ed5aad66
Stadnik Andrii <stadnikandriy1@gmail.com>
2025-12-06 15:56:20 +0200

    Update main.rs

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -1,36 +1,74 @@
 use anyhow::Result;
 use std::fs::read_to_string;
 
-use winnow::ascii::{dec_uint, space1};
-use winnow::combinator::{separated, separated_pair};
+use winnow::ascii::{dec_uint, space0, space1};
+use winnow::combinator::{delimited, separated, separated_pair, terminated};
 use winnow::{Parser, token::one_of};
 
-type T = u32;
-type Input = (Vec<Vec<T>>, Vec<Vec<char>>);
-fn parse_(input: &mut &str) -> winnow::Result<Input> {
+type T = u64;
+fn parse_(input: &mut &str) -> winnow::Result<(Vec<Vec<T>>, Vec<char>)> {
     let parse_row = separated(1.., dec_uint::<_, T, _>, space1).map(|v: Vec<T>| v);
-    let parse_nums = separated(1.., parse_row, "\n").map(|v: Vec<Vec<T>>| v);
-    let parse_op_row = separated(1.., one_of(['+', '*']), space1).map(|v: Vec<char>| v);
-    let parse_ops = separated(1.., parse_op_row, "\n").map(|v: Vec<Vec<char>>| v);
-    separated_pair(parse_nums, '\n', parse_ops).parse_next(input)
+    let parse_nums = separated(1.., delimited(space0, parse_row, space0), "\n");
+    let parse_ops = separated(1.., one_of(['+', '*']), space1);
+    separated_pair(parse_nums, '\n', terminated(parse_ops, space0)).parse_next(input)
 }
 
+type Input = Vec<(Vec<T>, char)>;
 fn parse(input: &str) -> Result<Input> {
-    parse_.parse(input).map_err(|e| anyhow::anyhow!("{e}"))
+    let (rows, ops) = parse_.parse(input).map_err(|e| anyhow::anyhow!("{e}"))?;
+    Ok((0..ops.len())
+        .map(|i| rows.iter().map(|row| row[i]).collect())
+        .zip(ops)
+        .collect())
 }
 
-fn task1(_input: &Input) -> Result<i32> {
-    Ok(0)
+fn task1(input: &Input) -> T {
+    input
+        .iter()
+        .map(|(row, op)| match op {
+            '+' => row.iter().sum::<T>(),
+            '*' => row.iter().product(),
+            _ => unreachable!(),
+        })
+        .sum()
 }
 
-fn task2(_input: &Input) -> Result<i32> {
-    Ok(0)
+fn task2(input: &str) -> Result<T> {
+    let data: Vec<_> = input.split('\n').map(|line| line.as_bytes()).collect();
+    let height = data.len();
+    let width = data[0].len();
+
+    let mut ans = 0;
+    let mut nums = Vec::new();
+    for col in (0..width).rev() {
+        let num = data[..height - 1]
+            .iter()
+            .filter_map(|row| row.get(col).filter(|&n| n.is_ascii_digit()))
+            .fold(0, |acc, n| acc * 10 + (n - b'0') as T);
+        if num != 0 {
+            nums.push(num);
+        }
+        match data[height - 1].get(col) {
+            Some(b'+') => {
+                ans += nums.iter().sum::<T>();
+                nums.clear();
+            }
+            Some(b'*') => {
+                ans += nums.iter().product::<T>();
+                nums.clear();
+            }
+            Some(b' ') | None => (),
+            _ => unreachable!(),
+        }
+    }
+    Ok(ans)
 }
 
 fn main() -> Result<()> {
-    let input = parse(&read_to_string("input.txt")?)?;
-    println!("Task 1: {}", task1(&input)?);
-    println!("Task 2: {}", task2(&input)?);
+    let s = read_to_string("input.txt")?;
+    let input = parse(&s)?;
+    println!("Task 1: {}", task1(&input));
+    println!("Task 2: {}", task2(&s)?);
     Ok(())
 }
 
@@ -38,25 +76,23 @@ fn main() -> Result<()> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r"123 328  51 64
-     45 64  387 23
-      6 98  215 314
-    *   +   *   +  ";
+    const INPUT: &str = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
 
     #[test]
     fn test_task1() -> Result<()> {
         let input = parse(INPUT)?;
-        assert_eq!(task1(&input)?, 4277556);
+        assert_eq!(task1(&input), 4277556);
         Ok(())
     }
 
     #[test]
     fn test_task2() -> Result<()> {
-        let input = parse(INPUT)?;
-        assert_eq!(task2(&input)?, 42);
+        assert_eq!(task2(&INPUT)?, 3263827);
         Ok(())
     }
 
     #[test]
-    fn test_main() -> Result<()> {}
+    fn test_main() -> Result<()> {
+        main()
+    }
 }
