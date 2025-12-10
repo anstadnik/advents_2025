bb4d95d923dc2aaeb6028ace10bde8226fd0d7f5
Stadnik Andrii <stadnikandriy1@gmail.com>
2025-12-10 14:56:08 +0200

    Update main.rs

--- d10/src/main.rs
+++ d10/src/main.rs
@@ -8,11 +8,12 @@ use std::usize;
 use winnow::combinator::{delimited, dispatch, empty, fail, repeat, separated, seq};
 use winnow::{Parser, ascii::dec_uint, token::take};
 
+type T = u16;
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Machine {
     lights: Vec<bool>,
     buttons: Vec<Vec<usize>>,
-    joltage: Vec<usize>,
+    joltage: Vec<T>,
 }
 
 fn parse_(input: &mut &str) -> winnow::Result<Vec<Machine>> {
@@ -34,7 +35,7 @@ fn parse_(input: &mut &str) -> winnow::Result<Vec<Machine>> {
         delimited('(', separated(1.., dec_uint::<_, usize, _>, ','), ')').map(|v: Vec<_>| v),
         ' ',
     );
-    let mut parse_joltage = delimited('{', separated(1.., dec_uint::<_, usize, _>, ','), '}');
+    let mut parse_joltage = delimited('{', separated(1.., dec_uint::<_, T, _>, ','), '}');
     let parse_machine = seq! {Machine {
         lights: parse_lights,
         _: ' ',
@@ -77,12 +78,17 @@ fn press_buttons(
     min_n_pressed: &mut usize,
     button_id: usize,
     buttons: &[Vec<usize>],
-    joltages: &mut Vec<usize>,
-    target: &[usize],
+    joltages: &mut [T],
+    target: &[T],
     joltages_sum: usize,
     target_sum: usize,
-    max_target: usize,
+    max_target: T,
 ) {
+    if target_sum == joltages_sum && joltages.iter().zip(target).all(|(&a, &b)| a == b) {
+        *min_n_pressed = n_pressed.min(*min_n_pressed);
+        return;
+    }
+
     if n_pressed >= *min_n_pressed
         || joltages_sum > target_sum
         || button_id >= buttons.len()
@@ -91,36 +97,25 @@ fn press_buttons(
         return;
     }
 
-    if target_sum == joltages_sum && joltages.iter().zip(target).all(|(&a, &b)| a == b) {
-        *min_n_pressed = n_pressed;
-        return;
-    }
-
     let button = &buttons[button_id];
     for n in 0..=max_target {
-        if n > 0 {
-            for &i in button.iter() {
-                joltages[i] += n;
-            }
-        }
-
         press_buttons(
-            n_pressed + n,
+            n_pressed + n as usize,
             min_n_pressed,
             button_id + 1,
             buttons,
             joltages,
             target,
-            joltages_sum + n * button.len(),
+            joltages_sum + n as usize * button.len(),
             target_sum,
             max_target,
         );
-
-        if n > 0 {
-            for &i in button.iter() {
-                joltages[i] -= n;
-            }
+        for &i in button {
+            joltages[i] += 1;
         }
+    }
+    for &i in button.iter() {
+        joltages[i] -= max_target + 1;
     }
 }
 
@@ -132,7 +127,7 @@ fn task2(input: Vec<Machine>) -> usize {
             m.buttons.sort_unstable_by_key(|v| v.len());
             m.buttons.reverse();
             let mut joltage = vec![0; m.lights.len()];
-            let mut min_n_pressed = usize::MAX;
+            let mut min_n_pressed = T::MAX as usize;
             press_buttons(
                 0,
                 &mut min_n_pressed,
@@ -141,7 +136,7 @@ fn task2(input: Vec<Machine>) -> usize {
                 &mut joltage,
                 &m.joltage,
                 0,
-                m.joltage.iter().sum(),
+                m.joltage.iter().map(|j| *j as usize).sum(),
                 *m.joltage.iter().max().unwrap(),
             );
             min_n_pressed
@@ -152,7 +147,7 @@ fn task2(input: Vec<Machine>) -> usize {
 fn main() -> Result<()> {
     let input = parse(&read_to_string("input.txt")?)?;
     println!("Task 1: {}", task1(&input));
-    println!("Task 2: {}", task2(input[..10].to_vec()));
+    println!("Task 2: {}", task2(input));
     Ok(())
 }
 
